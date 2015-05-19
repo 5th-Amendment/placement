92d51fc40c6d0f4102bab809099b616ba36317e0
Alan Yee <alyee@ucsd.edu>
2015-05-19 14:15:40 -0700

    Update doPlacement.py
    Improved readability

--- doPlacement.py
+++ doPlacement.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+
 import DataMover
 import CondorTools
 from IDPLException import *
@@ -10,13 +11,72 @@ import time
 import getopt
 import subprocess
 
-def sfString1():
-	
-def sfString2():
+def SF1HEADER():
+    ofile.write("############\n")
+    ofile.write("#\n")
+    ofile.write("# Parallel Job\n")
+    ofile.write("#\n")
+    ofile.write("############\n")
+    ofile.write("\n")
+    ofile.write("universe = parallel\n")
+    ofile.write("executable = placement2.py\n")
+    ofile.write("\n")
 
-def sfString3():
+def sf2Hosts(srcHost, dstHost):
+    ofile.write("SRC_HOST=%s\n" % srcHost)
+    ofile.write("SRC_PATH=/home/idpl/100M")
+    ofile.write("DST_HOST=%s\n" % dstHost)
+    ofile.write("DST_PATH=100M")
+    ofile.write("\n")
 
-def sfString4():
+def SF3ARGS():
+    ofile.write("# Keep running the job\n")
+    ofile.write("on_exit_remove=false\n")
+    ofile.write("\n")
+    ofile.write("# Arguments are:\n")
+    ofile.write("# 1. File to send (on the sending host)\n")
+    ofile.write("# 2. Location to write file (on the receiving host)\n")
+    ofile.write("\n")
+    ofile.write("arguments = -i $(SRC_PATH) -o $(DST_PATH)\n")
+    ofile.write("\n")
+    ofile.write("## Enable Chirp\n")
+    ofile.write("+WantIOProxy = true\n")
+    ofile.write("\n")
+
+def sf4IO(out, error, log):
+    ofile.write("input   = /dev/null\n")
+    ofile.write("output = %s.$(Node)\n" % out)
+    ofile.write("error  = %s.$(Node)\n" % error)
+    ofile.write("log    = %s\n" % log)
+    ofile.write("getenv = true\n")
+    ofile.write("\n")
+    ofile.write('+SrcPath = "$(SRC_PATH)"\n')
+    ofile.write('+DstHost = "$(DST_HOST)"\n')
+    ofile.write('+DstPath = "$(DST_PATH)"\n')
+    ofile.write("\n")
+
+def SF5POLICY():
+    ofile.write('+ParallelShutdownPolicy = "WAIT_FOR_ALL"\n')
+    ofile.write("\n")
+    ofile.write("transfer_input_files = DataMover.py,TimedExec.py,IDPLException.py,CondorTools.py,empty\n")
+    ofile.write("\n")
+    ofile.write("should_transfer_files = YES\n")
+    ofile.write("when_to_transfer_output = ON_EXIT\n")
+    ofile.write("\n")
+    ofile.write("machine_count = 1\n")
+    ofile.write('requirements = (Machine == "$(SRC_HOST)")\n')
+    ofile.write("transfer_output_files = empty\n")
+    ofile.write("queue\n")
+    ofile.write("\n")
+    ofile.write("machine_count = 1\n")
+    ofile.write('requirements = (Machine == "$(DST_HOST)")\n')
+    ofile.write("transfer_output_files = empty\n")
+    ofile.write("queue\n")
+    ofile.close()
+
+## *****************************
+## main routine
+## *****************************
 
 def main(argv):
     srcFile = ''
@@ -28,10 +88,11 @@ def main(argv):
     except getopt.GetoptError as err:
         print str(err) # 'Usage: --srcfile <sourcefile> --dstfile <destfile> 
         usage()        #         --srchost <sourcehost> --dsthost <desthost>'
-        sys.exit(4)
-    for opt, arg in options:
+        sys.exit(1)
+    for opt, arg in opts:
         if opt == '-h':
             print 'doPlacement.py -srcfile <sourcefile> --dstfile <destfile> --srchost <sourcehost> --dsthost <desthost>'
+            sys.exit()
         elif opt == '--srcfile':
             srcFile = arg
         elif opt == '--dstfile':
@@ -45,70 +106,20 @@ def main(argv):
     error  = dstFile + ".err"
     log    = dstFile + ".log"
     submit = dstFile + "-submit"
-    
-    ifile = open(srcFile)
-    catfile = ifile.read()
-    ofile = open(submit, "w+")
-    ofile.write("############\n")
-    ofile.write("#\n")
-    ofile.write("# Parallel Job\n")
-    ofile.write("#\n")
-    ofile.write("############\n")
-    ofile.write("\n")
-    ofile.write("universe = parallel\n")
-    ofile.write("executable = placement2.py\n")
-    ofile.write("\n")
-    
-    ofile.write("SRC_HOST=%s\n" % srcHost)
-    ofile.write("SRC_PATH=/home/idpl/100M")
-    ofile.write("DST_HOST=%s\n" % dstHost)
-    ofile.write("DST_PATH=100M")
-    ofile.write("\n")
+    try:
+        ifile = open(srcFile)
+        catfile = ifile.read()
+        ofile = open(submit, "w+")
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
 
-    ofile.write("# Keep running the job\n")
-    ofile.write("on_exit_remove=false\n")
-    ofile.write("\n")
-    ofile.write("# Arguments are:\n")
-    ofile.write("# 1. File to send (on the sending host)\n")
-    ofile.write("# 2. Location to write file (on the receiving host)\n")
-    ofile.write("\n")
-    ofile.write("arguments = -i $(SRC_PATH) -o $(DST_PATH)\n")
-    ofile.write("\n")
-    ofile.write("## Enable Chirp\n")
-    ofile.write("+WantIOProxy = true\n")
-    ofile.write("\n")
-    
-    ofile.write("input   = /dev/null\n")
-    ofile.write("output = %s.$(Node)\n" % out)
-    ofile.write("error  = %s.$(Node)\n" % error)
-    ofile.write("log    = %s\n" % log)
-    ofile.write("getenv = true\n")
-    ofile.write("\n")
-    ofile.write('+SrcPath = "$(SRC_PATH)"\n')
-    ofile.write('+DstHost = "$(DST_HOST)"\n')
-    ofile.write('+DstPath = "$(DST_PATH)"\n')
-    ofile.write("\n")
-    
-    ofile.write('+ParallelShutdownPolicy = "WAIT_FOR_ALL"\n')
-    ofile.write("\n")
-    ofile.write("transfer_input_files = DataMover.py,TimedExec.py,IDPLException.py,CondorTools.py,empty\n")
-    ofile.write("\n")
-    ofile.write("should_transfer_files = YES\n")
-    ofile.write("when_to_transfer_output = ON_EXIT\n")
-    ofile.write("\n")
-    ofile.write("machine_count = 1\n")
-    ofile.write('requirements = (Machine == "$(SRC_HOST)")\n')
-    ofile.write("transfer_output_files = empty\n")
-    ofile.write("queue\n")
-    ofile.write("\n")
-    
-    ofile.write("machine_count = 1\n")
-    ofile.write('requirements = (Machine == "$(DST_HOST)")\n')
-    ofile.write("transfer_output_files = empty\n")
-    ofile.write("queue\n")
-    ofile.close()
+    SF1HEADER()
+    sf2Hosts(srcHost, dstHost)
+    SF3ARGS()
+    sf4IO(out, error, log)
+    SF5POLICY()
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
 
 # vim: ts=4:sw=4:tw=78
